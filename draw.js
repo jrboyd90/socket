2416a0cc86fdae82c3510821f529f1c16346f4d3
Justin Boyd <jrboyd90@gmail.com>
2017-11-15 13:39:18 -0600

    draw

--- draw.js
+++ draw.js
@@ -14,33 +14,37 @@ app.use(express.static('public'));
 app.get('/', function (request, response) {
   response.render('draw.hbs');
 });
-// io.on('connection', function(client) {
-//
-//
-//   client.on('join-room', function(room){
-//     client.join(room, function() {
-//       console.log(client.rooms);
-//       io.to(room).emit('chat-msg', '**new user joined**');
-//     });
-//     if (ROOMS[room]) {
-//         ROOMS[room].push(client.id);
-//       } else {
-//         ROOMS[room] = [client.id];
-//       }
-//       console.log(ROOMS);
-//       ROOMS[room].forEach(function (id) {
-//         if (id != client.id) {
-//           io.to(id).emit('chat-msg', client.id + ' joined**');
-//         }
-//       });
-//       //io.to(client.id).emit('chat-msg', '**thanks for joining**');
-//     });
-//     client.on('incoming', function(msg){
-//       io.to(msg.room).emit('chat-msg', client.id + ': ' + msg.msg);
-//     });
-//
-//
-// });
+io.on('connection', function(client) {
+  console.log('Connected');
+
+
+  client.on('draw', function(coords){
+    console.log('Coords', coords);
+    io.emit('do-draw', coords);
+  });  
+    // client.join(room, function() {
+    //   console.log(client.rooms);
+    //   io.to(room).emit('chat-msg', '**new user joined**');
+    // });
+    // if (ROOMS[room]) {
+    //     ROOMS[room].push(client.id);
+    //   } else {
+    //     ROOMS[room] = [client.id];
+    //   }
+    //   console.log(ROOMS);
+    //   ROOMS[room].forEach(function (id) {
+    //     if (id != client.id) {
+    //       io.to(id).emit('chat-msg', client.id + ' joined**');
+    //     }
+    //   });
+    //   //io.to(client.id).emit('chat-msg', '**thanks for joining**');
+    // });
+    // client.on('incoming', function(msg){
+    //   io.to(msg.room).emit('chat-msg', client.id + ': ' + msg.msg);
+    // });
+
+
+});
 
 //starts up server on port 8888
 http.listen(8888, function () {
--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,6 +1,12 @@
 
-var mouse_down = false;
+
 var canvas = document.getElementById('canvas');
+var server  = io();
+var ctx = canvas.getContext('2d');
+
+var mouse_down = false;
+var past;
+var current;
 
 canvas.addEventListener('mousedown', function (event) {
   mouse_down = true;
@@ -15,5 +21,29 @@ canvas.addEventListener('mouseup', function (event) {
 canvas.addEventListener('mousemove', function (event) {
   if (mouse_down) {
     console.log('move', event.offsetX, event.offsetY);
+
+    current = [event.offsetX, event.offsetY];
+    if (past) {
+      send_draw(past, current);
+    }
+
+    past = [event.offsetX, event.offsetY];
   }
 });
+
+function send_draw (past, current) {
+  server.emit('draw', [past, current]);
+}
+server.on('do-draw', function (coords) {
+  draw(coords[0], coords[1]);
+});
+
+function draw (past, current) {
+  ctx.moveTo(past[0], past[1]);
+  ctx.quadraticCurveTo(
+    past[0], past[1],
+    current[0], current[1]
+  );
+  ctx.stroke();
+  ctx.closePath();
+}
